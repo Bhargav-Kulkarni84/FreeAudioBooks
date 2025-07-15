3c3956c14a0b609e5b402c19990cca56c1267cb3
Bhargav-Kulkarni84 <coderbhargav8237@gmail.com>
2025-07-15 23:07:09 +0530

    Updated Few Files

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ dotenv.config();
 
 module.exports = {
     MongoDB_URL:process.env.MongoDB_URL,
-    PORT:process.env.PORT
+    PORT:process.env.PORT,
+    MURF_API_KEY : process.env.MURF_API_KEY
 }
